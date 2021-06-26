2ab2767eea82f35a0440b66ee0d3e1316d509b1f
yonatankra <yonatan.kra@vonage.com>
2021-06-26 21:29:54 +0300

    Take 4

--- apps/house-wars-server/src/main.ts
+++ apps/house-wars-server/src/main.ts
@@ -7,8 +7,9 @@ import * as express from 'express';
 import { Server } from 'socket.io';
 import * as path from 'path';
 import { environment } from './environments/environment';
-const app = express();
 
+const port = process.env.PORT || 3333;
+const app = express();
 const STATIC = environment.staticPath;
 const INDEX = path.resolve(STATIC, 'index.html');
 
@@ -21,7 +22,6 @@ app.get('*', function (req, res) {
   res.sendFile(INDEX);
 });
 
-const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
